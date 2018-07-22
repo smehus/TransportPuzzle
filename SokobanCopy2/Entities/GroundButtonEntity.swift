88e83215745e1e2ab7c17e9e510ec15888e08af4
Scott Mehus <mehus.scott@gmail.com>
2018-07-22 14:44:56 -0600

    uggghh

--- SokobanCopy2/Entities/GroundButtonEntity.swift
+++ SokobanCopy2/Entities/GroundButtonEntity.swift
@@ -32,8 +32,20 @@ final class GroundButtonEntity: GKEntity {
 }
 
 final class GroundButtonCollisionComponent: GKComponent {
-    var hasContact = false
+    var currentCollision: SCNNode?
     
+    override func update(deltaTime seconds: TimeInterval) {
+        super.update(deltaTime: seconds)
+        guard let node = entity?.component(ofType: GKSCNNodeComponent.self)?.node else { return }
+        
+        if let collision = currentCollision {
+            let offset = node.presentation.simdPosition - collision.presentation.simdPosition
+            if abs(offset.x) > 1.9 || abs(offset.z) > 1.9 {
+                currentCollision = nil
+                print("NO LONGER INTERSECTING ****")
+            }
+        }
+    }
 }
 
 extension GroundButtonCollisionComponent: CollisionDetector {
@@ -43,15 +55,26 @@ extension GroundButtonCollisionComponent: CollisionDetector {
         let collision = contact.nodeA.physicsBody!.categoryBitMask == ColliderType.groundButton.categoryMask ? ColliderType(rawValue: contact.nodeB.physicsBody!.categoryBitMask) : ColliderType(rawValue: contact.nodeA.physicsBody!.categoryBitMask)
         
         guard groundCollider == .groundButton else { return }
+        guard currentCollision == nil else { return }
         
-        if groundCollider.notifyOnContactWith(collision), !hasContact {
-            hasContact = true
-            print("handle collision!!!!! for button!!!! ")
+        if groundCollider.notifyOnContactWith(collision) {
+            print("ADDING COLLISION ***")
+            currentCollision = contact.nodeA.physicsBody!.categoryBitMask == ColliderType.groundButton.categoryMask ? contact.nodeB : contact.nodeA
         }
     }
     
     func didEnd(_ contact: SCNPhysicsContact) {
         // Getting called cause certain parts of armature get not touching anymore??
-        print("CONTACT DID KENDDDDDDD")
+        
+        /*
+        let groundCollider = contact.nodeA.physicsBody!.categoryBitMask == ColliderType.groundButton.categoryMask ? ColliderType(rawValue: contact.nodeA.physicsBody!.categoryBitMask) : ColliderType(rawValue: contact.nodeB.physicsBody!.categoryBitMask)
+        let collision = contact.nodeA.physicsBody!.categoryBitMask == ColliderType.groundButton.categoryMask ? ColliderType(rawValue: contact.nodeB.physicsBody!.categoryBitMask) : ColliderType(rawValue: contact.nodeA.physicsBody!.categoryBitMask)
+        
+        guard groundCollider == .groundButton else { return }
+        
+        if groundCollider.notifyOnContactWith(collision) {
+            currentCollision = contact.nodeA.physicsBody!.categoryBitMask == ColliderType.groundButton.categoryMask ? contact.nodeB : contact.nodeA
+        }
+     */
     }
 }
